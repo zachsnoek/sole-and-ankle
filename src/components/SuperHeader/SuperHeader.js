cc7e4a3cfbc46b73ff9d4f150ef5910ce553f642
zachsnoek <dev@zachsnoek.com>
2022-03-08 15:52:40 -0800

    Complete exercise 1.

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -8,38 +8,43 @@ import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 
 const SuperHeader = () => {
-  return (
-    <Wrapper>
-      <MarketingMessage>
-        Free shipping on domestic orders over $75!
-      </MarketingMessage>
-      <SearchInput />
-      <HelpLink href="/help">Help</HelpLink>
-      <UnstyledButton>
-        <Icon id="shopping-bag" strokeWidth={1} />
-      </UnstyledButton>
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <MarketingMessage>
+                Free shipping on domestic orders over $75!
+            </MarketingMessage>
+            <SearchInput />
+            <HelpLink href="/help">Help</HelpLink>
+            <UnstyledButton>
+                <Icon id="shopping-bag" strokeWidth={1} />
+            </UnstyledButton>
+        </Wrapper>
+    );
 };
 
 const Wrapper = styled.div`
-  font-size: 0.875rem;
-  color: ${COLORS.gray[300]};
-  background-color: ${COLORS.gray[900]};
+    display: flex;
+    align-items: center;
+    padding: 12px 32px;
+    gap: 24px;
+    font-size: 0.875rem;
+    color: ${COLORS.gray[300]};
+    background-color: ${COLORS.gray[900]};
 `;
 
 const MarketingMessage = styled.span`
-  color: ${COLORS.white};
+    color: ${COLORS.white};
+    margin-right: auto;
 `;
 
 const HelpLink = styled.a`
-  color: inherit;
-  text-decoration: none;
-  outline-offset: 2px;
+    color: inherit;
+    text-decoration: none;
+    outline-offset: 2px;
 
-  &:not(:focus-visible) {
-    outline: none;
-  }
+    &:not(:focus-visible) {
+        outline: none;
+    }
 `;
 
 export default SuperHeader;
