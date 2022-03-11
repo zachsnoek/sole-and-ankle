f918fd19d56cd75420dcf0c913989897c2f6c9e9
zachsnoek <dev@zachsnoek.com>
2022-03-11 15:32:33 -0800

    Attempt shoe grid.

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -6,81 +6,86 @@ import { formatPrice, pluralize, isNewShoe } from '../../utils';
 import Spacer from '../Spacer';
 
 const ShoeCard = ({
-  slug,
-  name,
-  imageSrc,
-  price,
-  salePrice,
-  releaseDate,
-  numOfColors,
+    slug,
+    name,
+    imageSrc,
+    price,
+    salePrice,
+    releaseDate,
+    numOfColors,
 }) => {
-  // There are 3 variants possible, based on the props:
-  //   - new-release
-  //   - on-sale
-  //   - default
-  //
-  // Any shoe released in the last month will be considered
-  // `new-release`. Any shoe with a `salePrice` will be
-  // on-sale. In theory, it is possible for a shoe to be
-  // both on-sale and new-release, but in this case, `on-sale`
-  // will triumph and be the variant used.
-  // prettier-ignore
-  const variant = typeof salePrice === 'number'
+    // There are 3 variants possible, based on the props:
+    //   - new-release
+    //   - on-sale
+    //   - default
+    //
+    // Any shoe released in the last month will be considered
+    // `new-release`. Any shoe with a `salePrice` will be
+    // on-sale. In theory, it is possible for a shoe to be
+    // both on-sale and new-release, but in this case, `on-sale`
+    // will triumph and be the variant used.
+    // prettier-ignore
+    const variant = typeof salePrice === 'number'
     ? 'on-sale'
     : isNewShoe(releaseDate)
       ? 'new-release'
       : 'default'
 
-  return (
-    <Link href={`/shoe/${slug}`}>
-      <Wrapper>
-        <ImageWrapper>
-          <Image alt="" src={imageSrc} />
-        </ImageWrapper>
-        <Spacer size={12} />
-        <Row>
-          <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
-        </Row>
-        <Row>
-          <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
-        </Row>
-      </Wrapper>
-    </Link>
-  );
+    return (
+        <Link href={`/shoe/${slug}`}>
+            <Wrapper>
+                <ImageWrapper>
+                    <Image alt="" src={imageSrc} />
+                </ImageWrapper>
+                <Spacer size={12} />
+                <Row>
+                    <Name>{name}</Name>
+                    <Price>{formatPrice(price)}</Price>
+                </Row>
+                <Row>
+                    <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+                </Row>
+            </Wrapper>
+        </Link>
+    );
 };
 
 const Link = styled.a`
-  text-decoration: none;
-  color: inherit;
+    text-decoration: none;
+    color: inherit;
 `;
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+    flex: 1 1 240px;
+    max-width: 340px;
+`;
 
 const ImageWrapper = styled.div`
-  position: relative;
+    position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+    max-width: 100%;
+`;
 
 const Row = styled.div`
-  font-size: 1rem;
+    font-size: 1rem;
 `;
 
 const Name = styled.h3`
-  font-weight: ${WEIGHTS.medium};
-  color: ${COLORS.gray[900]};
+    font-weight: ${WEIGHTS.medium};
+    color: ${COLORS.gray[900]};
 `;
 
 const Price = styled.span``;
 
 const ColorInfo = styled.p`
-  color: ${COLORS.gray[700]};
+    color: ${COLORS.gray[700]};
 `;
 
 const SalePrice = styled.span`
-  font-weight: ${WEIGHTS.medium};
-  color: ${COLORS.primary};
+    font-weight: ${WEIGHTS.medium};
+    color: ${COLORS.primary};
 `;
 
 export default ShoeCard;
--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -5,15 +5,20 @@ import SHOES from '../../data';
 import ShoeCard from '../ShoeCard';
 
 const ShoeGrid = () => {
-  return (
-    <Wrapper>
-      {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
-      ))}
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            {SHOES.map((shoe) => (
+                <ShoeCard key={shoe.slug} {...shoe} />
+            ))}
+        </Wrapper>
+    );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    gap: 32px;
+`;
 
 export default ShoeGrid;
